df86b4657e595fcbf22d0e4b9e09d7c4a6d89aa4
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-20 19:29:05 -0500

    add stretch, single like only

--- frontend/components/Posts/LikeSection.js
+++ frontend/components/Posts/LikeSection.js
@@ -2,11 +2,21 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
+import React, { useState } from 'react';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
+  const [ hasLiked, setHasLiked ] = useState(false); // tracks whether user has liked a post previously
+
+  const handleLikes = () => {
+    if (!hasLiked) {
+      likePost();
+      setHasLiked(true);
+    } else {
+      alert('Already liked this post!');
+    }
+  }
 
   return (
     <div>
@@ -14,7 +24,7 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper' onClick={likePost}>
+        <div className='like-section-wrapper' onClick={handleLikes}>
           ❤️
         </div>
         <div className='like-section-wrapper'>
